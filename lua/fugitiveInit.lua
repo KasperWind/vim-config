6b1928ec9cf46e56d0e9c3f15369b1728f53a543
Kasper Wind <kasperwind79@gmail.com>
2022-01-08 19:58:55 +0100

    updating fugitive keymappings, adding vimspector defauls and keymappings

--- lua/fugitiveInit.lua
+++ lua/fugitiveInit.lua
@@ -1,2 +1,4 @@
 
-vim.api.nvim_set_keymap('n', '<leader>c', ':Git commit<cr>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>cc', ':Git commit<cr>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>cd', ':G<cr>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>cp', ':Git push<cr>', { noremap = true, silent = true})
--- lua/vimspectorInit.lua
+++ lua/vimspectorInit.lua
@@ -1,1 +1,3 @@
+vim.cmd('packadd! vimspector')
+vim.g.vimspector_enable_mappings='HUMAN'
 vim.g.vimspector_install_gadgets = { 'debugpy', 'vscode-cpptools', 'CodeLLDB' }
