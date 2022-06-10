7683b3454088ec53a5fcbfa6487bf446db996f75
Kasper Wind <kasperwind79@gmail.com>
2022-06-10 21:51:32 +0200

    fixing fugitive

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -9,6 +9,6 @@ vim.cmd('highlight LineNr ctermfg=yellow')
 --Set statusbar
 vim.g.lightline = { colorscheme = 'onedark';
       active = { left = { { 'mode', 'paste' }, { 'gitbranch', 'readonly', 'filename', 'modified' } } };
-      component_function = { gitbranch = 'fugitive#head', };
+      component_function = { gitbranch = 'FugitiveHead', };
 }
 
