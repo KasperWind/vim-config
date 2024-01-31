ea64f37e8aefdc5e770e483ae6dfcb878019bda4
Kasper Wind <kasperwind79@gmail.com>
2024-01-31 15:34:26 +0100

    added basic lsp functionality

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,15 +1,23 @@
 return {
-  "folke/neodev.nvim",
-  "folke/which-key.nvim",
-  { 
-      "folke/neoconf.nvim",
-      cmd = "Neoconf" 
-  },
-  { "catppuccin/nvim", 
-      name = "catppuccin",
-      priority = 1000,
-      config = function()
-        vim.cmd.colorscheme "catppuccin"
-      end
-  },
+    "folke/neodev.nvim",
+    "folke/which-key.nvim",
+    {
+        "folke/neoconf.nvim",
+        cmd = "Neoconf"
+    },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        config = function()
+            vim.cmd.colorscheme "catppuccin"
+        end
+    },
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            require("lualine").setup()
+        end
+    },
 }
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -3,12 +3,46 @@ local ensure_installed = {
 }
 return {
     "neovim/nvim-lspconfig",
-    { 
+    {
         "williamboman/mason-lspconfig.nvim",
+        dependencies = {
+            "hrsh7th/nvim-cmp",
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-nvim-lua",
+            "hrsh7th/cmp-buffer", -- buffer completions
+            "hrsh7th/cmp-path", -- path completions
+            "hrsh7th/cmp-cmdline", -- cmdline completions
+            "saadparwaiz1/cmp_luasnip", -- snippet completions
+        },
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = ensure_installed
+                ensure_installed = ensure_installed,
+                automatic_installation = true,
             })
+            local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
+            if not lspconfig_status_ok then
+                return
+            end
+
+            local opts = {}
+
+            local handlers = require("lsp.handlers")
+
+            for _, server in pairs(ensure_installed) do
+                opts = {
+                    on_attach = handlers.on_attach,
+                    capabilities = handlers.capabilities,
+                }
+
+                server = vim.split(server, "@")[1]
+
+                local require_ok, conf_opts = pcall(require, "lsp.settings." .. server)
+                if require_ok then
+                    opts = vim.tbl_deep_extend("force", conf_opts, opts)
+                end
+
+                lspconfig[server].setup(opts)
+            end
         end
     },
     {
@@ -23,6 +57,7 @@ return {
                     }
                 }
             })
+
         end
     }
 }
