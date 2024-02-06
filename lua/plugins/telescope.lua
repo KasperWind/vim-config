dbb43f5991b074d3c2f012450fe261084b96f843
Kasper Wind <kasperwind79@gmail.com>
2024-02-06 09:25:13 +0100

    updated telescope setup

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,58 +1,72 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
-      dependencies = { 'nvim-lua/plenary.nvim',
-      config = function()
-        -- Enable telescope fzf native, if installed
-        pcall(require('telescope').load_extension, 'fzf')
-        pcall(require('telescope').load_extension, 'media_files')
+    {
+        'nvim-telescope/telescope-fzf-native.nvim', build = 'make',
+    },
+    {
+        'nvim-telescope/telescope.nvim', tag = '0.1.5',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            'nvim-telescope/telescope-fzf-native.nvim',
+        },
+        config = function()
+            -- Enable telescope fzf native, if installed
 
-        -- See `:help telescope.builtin`
-        vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-        vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
-        vim.keymap.set('n', '<leader>/', function()
-          -- You can pass additional configuration to telescope to change theme, layout, etc.
-          require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-            winblend = 10,
-            previewer = false,
-          })
-        end, { desc = '[/] Fuzzily search in current buffer' })
+            -- See `:help telescope.builtin`
+            vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
+            vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+            vim.keymap.set('n', '<leader>/', function()
+                -- You can pass additional configuration to telescope to change theme, layout, etc.
+                require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+                    winblend = 10,
+                    previewer = false,
+                })
+                end, { desc = '[/] Fuzzily search in current buffer' })
 
-        vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-        vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-        vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-        vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
-        vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
-        vim.keymap.set('n', '<leader>sr', require('telescope.builtin').git_files, { desc = '[S]earch by git [R]epo' })
+            vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+            vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
+            vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
+            vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
+            vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+            vim.keymap.set('n', '<leader>sr', require('telescope.builtin').git_files, { desc = '[S]earch by git [R]epo' })
 
-        local actions = require "telescope.actions"
+            local actions = require "telescope.actions"
 
-        require("telescope").setup {
-          defaults = {
+            require("telescope").setup {
+                defaults = {
 
-            prompt_prefix = " ",
-            selection_caret = " ",
-            path_display = { "smart" },
-            file_ignore_patterns = { ".git/", "node_modules", "target" },
+                prompt_prefix = " ",
+                selection_caret = " ",
+                path_display = { "smart" },
+                file_ignore_patterns = { ".git/", "node_modules", "target", "build" },
 
-            mappings = {
-              i = {
-                ["<Down>"] = actions.cycle_history_next,
-                ["<Up>"] = actions.cycle_history_prev,
-                ["<C-j>"] = actions.move_selection_next,
-                ["<C-k>"] = actions.move_selection_previous,
-              },
-            },
-          },
-          extensions = {
-            media_files = {
-              -- filetypes whitelist
-              -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
-              filetypes = {"png", "webp", "jpg", "jpeg"},
-              -- find command (defaults to `fd`)
-              find_cmd = "fd"
+                mappings = {
+                    i = {
+                        ["<Down>"] = actions.cycle_history_next,
+                        ["<Up>"] = actions.cycle_history_prev,
+                        ["<C-j>"] = actions.move_selection_next,
+                        ["<C-k>"] = actions.move_selection_previous,
+                        },
+                    },
+                },
+                extensions = {
+                    media_files = {
+                        -- filetypes whitelist
+                        -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
+                        filetypes = {"png", "webp", "jpg", "jpeg"},
+                        -- find command (defaults to `fd`)
+                        find_cmd = "fd"
+                    },
+                    fzf = {
+                        fuzzy = true,
+                        override_generic_sorter = true,
+                        override_file_sorter = true,
+                        case_mode = "smart_case",
+                    }
+                },
             }
-          },
-        }
-      end,
-  },
+
+            pcall(require('telescope').load_extension, 'fzf')
+            pcall(require('telescope').load_extension, 'media_files')
+        end,
+    },
 }
