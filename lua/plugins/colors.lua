3ae003a26e569f881c17ce7a76313d16ec41c1bd
Kasper Wind <kasperwind79@gmail.com>
2024-02-10 23:25:12 +0100

    adding kanagawa colorscheme

---
+++ lua/plugins/colors.lua
@@ -0,0 +1,19 @@
+return {
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        -- config = function()
+        --     vim.cmd.colorscheme "catppuccin"
+        -- end
+    },
+    {
+        "rebelot/kanagawa.nvim",
+        name = "kanagawa",
+        priority = 1000,
+        config = function()
+            vim.cmd.colorscheme "kanagawa"
+        end
+    },
+
+}
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -12,14 +12,6 @@ return {
         cmd = "Neoconf"
     },
     {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000,
-        config = function()
-            vim.cmd.colorscheme "catppuccin"
-        end
-    },
-    {
         "nvim-lualine/lualine.nvim",
         dependencies = { "nvim-tree/nvim-web-devicons" },
         config = function()
