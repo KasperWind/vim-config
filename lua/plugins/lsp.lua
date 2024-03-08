7e5d7e8dedf930678cb8afef0d59e85fdaafe351
Kasper Wind <kasperwind79@gmail.com>
2024-03-08 22:04:51 +0100

    update js-lint

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -93,15 +93,15 @@ return {
         end
     },
     {
-        "quick-lint/quick-lint-js",
-        cond = function(plugin)
-            -- TODO(strager): Don't make this happen multiple times.
-            plugin.dir = plugin.dir .. "/plugin/vim/quick-lint-js.vim"
-            return true
-        end,
-        config = function()
-            require("lspconfig/quick_lint_js").setup {}
-        end,
+--        "quick-lint/quick-lint-js",
+--        cond = function(plugin)
+--            -- TODO(strager): Don't make this happen multiple times.
+--            plugin.dir = plugin.dir .. "/plugin/vim/quick-lint-js.vim"
+--            return true
+--        end,
+--        config = function()
+--            require("lspconfig/quick_lint_js").setup {}
+--        end,
     },
     {
         "folke/trouble.nvim",
