557397ef30210c57558b02482a6b535cdf2b8d31
Kasper Wind <kasperwind79@gmail.com>
2024-04-14 16:47:07 +0200

    Adding spectre search and replace

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -57,19 +57,41 @@ return {
         end
     },
     {
-        'numToStr/Comment.nvim',
+        "numToStr/Comment.nvim",
         opts = {},
         lazy = false,
     },
     {
-        'fei6409/log-highlight.nvim',
+        "fei6409/log-highlight.nvim",
         config = function()
-            require('log-highlight').setup {}
+            require("log-highlight").setup {}
         end,
     },
     {
-        'stevearc/oil.nvim',
+        "stevearc/oil.nvim",
         opts = {},
         dependencies = { "nvim-tree/nvim-web-devicons" },
     },
+    {
+        "nvim-pack/nvim-spectre",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        keys = {
+            { "<leader>S", "<cmd>lua require('spectre').toggle()<CR>", desc = "Toggle spectre", mode = { "n", "v" } },
+            { "<leader>sw", "<cmd>lua require('spectre').open_visual({select_word=true})<CR>", desc = "Search current word", mode = { "n", "v" } },
+            { "<leader>sp", "<cmd>lua require('spectre').open_file_search({select_word=true})<CR>", desc = "Search on current file", mode = { "n", "v" } },
+        },
+    },
 }
+
+-- vim.keymap.set('n', '<leader>S', '<cmd>lua require("spectre").toggle()<CR>', {
+--     desc = "Toggle Spectre"
+-- })
+-- vim.keymap.set('n', '<leader>sw', '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
+--     desc = "Search current word"
+-- })
+-- vim.keymap.set('v', '<leader>sw', '<esc><cmd>lua require("spectre").open_visual()<CR>', {
+--     desc = "Search current word"
+-- })
+-- vim.keymap.set('n', '<leader>sp', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
+--     desc = "Search on current file"
+-- })
