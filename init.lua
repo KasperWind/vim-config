e670e711ff720225c9ed44474c6bb571084ee543
Kasper Wind <kasperwind79@gmail.com>
2024-04-14 17:44:37 +0200

    adding desc to keymapping

--- init.lua
+++ init.lua
@@ -3,14 +3,14 @@ vim.g.maplocalleader = " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -23,4 +23,3 @@ require("filetypes")
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
-
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,12 +1,17 @@
 local opts = { noremap = true, silent = true }
-
 local term_opts = { silent = true }
+
+local get_opts = function(desc)
+    local t = opts
+    t.desc = desc
+    return t
+end
 
 -- Shorten function name
 local keymap = vim.api.nvim_set_keymap
 
 --Remap space as leader key
-keymap("", "<Space>", "<Nop>", opts)
+keymap("", "<Space>", "<Nop>", get_opts(""))
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
@@ -20,46 +25,46 @@ vim.g.maplocalleader = " "
 
 -- Normal --
 -- Better window navigation
-keymap("n", "<C-h>", "<C-w>h", opts)
-keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
+keymap("n", "<C-h>", "<C-w>h", get_opts(""))
+keymap("n", "<C-j>", "<C-w>j", get_opts(""))
+keymap("n", "<C-k>", "<C-w>k", get_opts(""))
+keymap("n", "<C-l>", "<C-w>l", get_opts(""))
 
 -- Resize with arrows
-keymap("n", "<C-Up>", ":resize +2<CR>", opts)
-keymap("n", "<C-Down>", ":resize -2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+keymap("n", "<C-Up>", ":resize +2<CR>", get_opts(""))
+keymap("n", "<C-Down>", ":resize -2<CR>", get_opts(""))
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", get_opts(""))
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", get_opts(""))
 
 -- Close buffers
-keymap("n", "<S-q>", "<cmd>bdelete!<CR>", opts)
+keymap("n", "<S-q>", "<cmd>bdelete!<CR>", get_opts(""))
 
 -- Navigate buffers
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
+keymap("n", "<S-l>", ":bnext<CR>", get_opts(""))
+keymap("n", "<S-h>", ":bprevious<CR>", get_opts(""))
 
 -- NvimTree
---keymap("n", "<leader>n", ":NvimTreeToggle<CR>", opts)
+--keymap("n", "<leader>n", ":NvimTreeToggle<CR>", get_opts(""))
 
 -- Oil
-keymap("n", "<leader>n", ":Oil<CR>", opts)
+keymap("n", "<leader>n", ":Oil<CR>", get_opts(""))
 
 -- Visual --
 -- Stay in indent mode
-keymap("v", "<", "<gv", opts)
-keymap("v", ">", ">gv", opts)
+keymap("v", "<", "<gv", get_opts(""))
+keymap("v", ">", ">gv", get_opts(""))
 
 -- Move text up and down
-keymap("v", "<A-j>", ":m .+1<CR>==", opts)
-keymap("v", "<A-k>", ":m .-2<CR>==", opts)
-keymap("v", "p", '"_dP', opts)
+keymap("v", "<A-j>", ":m .+1<CR>==", get_opts(""))
+keymap("v", "<A-k>", ":m .-2<CR>==", get_opts(""))
+keymap("v", "p", '"_dP', get_opts(""))
 
 -- Visual Block --
 -- Move text up and down
-keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
-keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "J", ":move '>+1<CR>gv-gv", get_opts(""))
+keymap("x", "K", ":move '<-2<CR>gv-gv", get_opts(""))
+keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", get_opts(""))
+keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", get_opts(""))
 
 -- Terminal --
 -- Better terminal navigation
@@ -70,19 +75,18 @@ keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
 keymap("t", "<ESC>", "<C-\\><C-n>", term_opts)
 
 -- Quit
-keymap('n', '<C-q>', ':q<CR>', opts)
-keymap('n', '<C-ESC>', ':q<CR>', opts)
+keymap('n', '<C-q>', ':q<CR>', get_opts(""))
+keymap('n', '<C-ESC>', ':q<CR>', get_opts(""))
 
 -- Keeps screen for jumping aorund
-keymap("n", "J", "mzJ`z", opts)
-keymap("n", "<C-d>", "<C-d>zz", opts)
-keymap("n", "<C-u>", "<C-u>zz", opts)
-keymap("n", "n", "nzzzv", opts)
-keymap("n", "N", "Nzzzv", opts)
+keymap("n", "J", "mzJ`z", get_opts(""))
+keymap("n", "<C-d>", "<C-d>zz", get_opts(""))
+keymap("n", "<C-u>", "<C-u>zz", get_opts(""))
+keymap("n", "n", "nzzzv", get_opts(""))
+keymap("n", "N", "Nzzzv", get_opts(""))
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
-
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, get_opts(""))
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, get_opts(""))
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, get_opts(""))
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, get_opts(""))
