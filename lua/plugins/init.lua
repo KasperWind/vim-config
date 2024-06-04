d4de2648bc09ccaba6f16195d9c7dd212fe34cc8
Kasper Wind <kasperwind79@gmail.com>
2024-06-04 13:39:05 +0200

    adding keymap for opening nvim oil in dual pane view

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -82,6 +82,21 @@ return {
                 ["<C-r>"] = "actions.refresh",
             }
         },
+        keys = {
+            { "<leader>ns",
+                function() 
+                    require('oil').open() 
+                    local start = vim.api.nvim_get_current_win()
+                    vim.cmd('vsplit')
+                    local win = vim.api.nvim_get_current_win()
+                    local buf = vim.api.nvim_create_buf(true, true) 
+                    vim.api.nvim_win_set_buf(win, buf)
+                    local pwd = vim.fn.getcwd()
+                    require('oil').open(pwd) 
+                    vim.api.nvim_set_current_win(start)
+                end, 
+                desc = "Oil.nvim open split view", mode = { "n", "v" } },
+        },
         dependencies = { "nvim-tree/nvim-web-devicons" },
     },
     {
