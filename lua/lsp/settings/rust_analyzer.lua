88b041a78d07f21f734bc3d3a47ae39c5306d111
Kasper Wind <kasperwind79@gmail.com>
2024-08-01 20:57:04 +0200

    change rust build to buildin compiler cmd

--- lua/lsp/settings/rust_analyzer.lua
+++ lua/lsp/settings/rust_analyzer.lua
@@ -16,9 +16,9 @@ return {
         local opts = function (desc)
             return { desc=desc, noremap = true, silent = true }
         end
-        vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>bb", "<cmd>make build<CR>", opts("runs :make Build"))
-        vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>br", "<cmd>make run<CR>", opts("runs: :make run"))
-        vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>bt", "<cmd>make test<CR>", opts("runs: :make test"))
+        vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>bb", "<cmd>make build<CR>", opts("Cargo build"))
+        vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>br", "<cmd>make run<CR>", opts("Cargo run"))
+        vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>bt", "<cmd>make test<CR>", opts("Cargo test"))
         handlers.on_attach_required(client, bufnr)
     end,
 	settings = {
