d6729be1ae1631d33d6af6ea62cde662d606823e
Kasper Wind <kasperwind79@gmail.com>
2025-03-24 09:39:05 +0100

    setup dap for dotnet to very specific setup, but works

---
+++ lua/plugins/debug.lua
@@ -0,0 +1,73 @@
+return {
+    {
+        'mfussenegger/nvim-dap',
+        config = function()
+            local dap = require('dap')
+            local dapui = require('dapui')
+
+            dap.adapters.coreclr = {
+                type = 'executable',
+                command = 'C:\\Users\\extkawi\\Downloads\\netcoredbg-win64\\netcoredbg\\netcoredbg.exe',
+                args = { '--interpreter=vscode' }
+            }
+            dap.configurations.cs = {
+                {
+                    type = "coreclr",
+                    name = "launch - netcoredbg",
+                    request = "launch",
+                    program = function()
+                        return vim.fn.input('Path to dll:', vim.fn.getcwd() .. '/bin/Debug/', 'file')
+                    end,
+                },
+            }
+            dap.listeners.before.attach.dapui_config = function()
+                dapui.open()
+            end
+            dap.listeners.before.launch.dapui_config = function()
+                dapui.open()
+            end
+            dap.listeners.before.event_terminated.dapui_config = function()
+                dapui.close()
+            end
+            dap.listeners.before.event_exited.dapui_config = function()
+                dapui.close()
+            end
+        end
+    },
+    {
+        "rcarriga/nvim-dap-ui",
+        dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+        config = function()
+            require('dapui').setup({
+                layouts = { {
+                    elements = { {
+                        id = "scopes",
+                        size = 0.25
+                    }, {
+                        id = "breakpoints",
+                        size = 0.25
+                    }, {
+                        id = "stacks",
+                        size = 0.25
+                    }, {
+                        id = "watches",
+                        size = 0.25
+                    } },
+                    position = "left",
+                    size = 70
+                }, {
+                    elements = { {
+                        id = "repl",
+                        size = 0.5
+                    }, {
+                        id = "console",
+                        size = 0.5
+                    } },
+                    position = "bottom",
+                    size = 10
+                } },
+
+            })
+        end
+    }
+}
