367c8267a9bdb24dd9fa26f5d795b6fac98e8e50
Kasper Wind <kasperwind79@gmail.com>
2025-07-31 23:51:31 +0200

    created basic clean version

--- init.lua
+++ init.lua
@@ -1,66 +1,467 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-vim.deprecate = function () end
+-- theme & transparency
+vim.cmd.colorscheme("habamax")
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
+-- Basic settings
+vim.opt.number = true                              -- Line numbers
+vim.opt.relativenumber = true                      -- Relative line numbers
+vim.opt.cursorline = true                          -- Highlight current line
+vim.opt.wrap = false                               -- Don't wrap lines
+vim.opt.scrolloff = 10                             -- Keep 10 lines above/below cursor 
+vim.opt.sidescrolloff = 8                          -- Keep 8 columns left/right of cursor
+
+-- Indentation
+vim.opt.tabstop = 4                                -- Tab width
+vim.opt.shiftwidth = 4                             -- Indent width
+vim.opt.softtabstop = 4                            -- Soft tab stop
+vim.opt.expandtab = true                           -- Use spaces instead of tabs
+vim.opt.smartindent = true                         -- Smart auto-indenting
+vim.opt.autoindent = true                          -- Copy indent from current line
+
+-- Search settings
+vim.opt.ignorecase = true                          -- Case insensitive search
+vim.opt.smartcase = true                           -- Case sensitive if uppercase in search
+vim.opt.hlsearch = false                           -- Don't highlight search results 
+vim.opt.incsearch = true                           -- Show matches as you type
+
+-- Visual settings
+vim.opt.termguicolors = true                       -- Enable 24-bit colors
+vim.opt.signcolumn = "yes"                         -- Always show sign column
+vim.opt.numberwidth = 3                            -- set number column width
+vim.opt.colorcolumn = "100"                        -- Show column at 100 characters
+vim.opt.showmatch = true                           -- Highlight matching brackets
+vim.opt.matchtime = 2                              -- How long to show matching bracket
+vim.opt.cmdheight = 1                              -- Command line height
+vim.opt.completeopt = "menuone,noinsert,noselect"  -- Completion options 
+vim.opt.showmode = false                           -- Don't show mode in command line 
+vim.opt.pumheight = 10                             -- Popup menu height 
+vim.opt.pumblend = 10                              -- Popup menu transparency 
+vim.opt.winblend = 0                               -- Floating window transparency 
+vim.opt.conceallevel = 0                           -- Don't hide markup 
+vim.opt.concealcursor = ""                         -- Don't hide cursor line markup 
+vim.opt.lazyredraw = true                          -- Don't redraw during macros
+vim.opt.synmaxcol = 300                            -- Syntax highlighting limit
+vim.opt.colorcolumn = "110"
+
+-- File handling
+vim.opt.backup = false                             -- Don't create backup files
+vim.opt.writebackup = false                        -- Don't create backup before writing
+vim.opt.swapfile = false                           -- Don't create swap files
+vim.opt.undofile = true                            -- Persistent undo
+vim.opt.undodir = vim.fn.expand("~/.vim/undodir")  -- Undo directory
+vim.opt.updatetime = 300                           -- Faster completion
+vim.opt.timeoutlen = 500                           -- Key timeout duration
+vim.opt.ttimeoutlen = 0                            -- Key code timeout
+vim.opt.autoread = true                            -- Auto reload files changed outside vim
+vim.opt.autowrite = false                          -- Don't auto save
+
+-- Behavior settings
+vim.opt.hidden = true                              -- Allow hidden buffers
+vim.opt.errorbells = false                         -- No error bells
+vim.opt.backspace = "indent,eol,start"             -- Better backspace behavior
+vim.opt.autochdir = false                          -- Don't auto change directory
+vim.opt.iskeyword:append("-")                      -- Treat dash as part of word
+vim.opt.path:append("**")                          -- include subdirectories in search
+vim.opt.selection = "exclusive"                    -- Selection behavior
+vim.opt.mouse = "a"                                -- Enable mouse support
+vim.opt.clipboard:append("unnamedplus")            -- Use system clipboard
+vim.opt.modifiable = true                          -- Allow buffer modifications
+vim.opt.encoding = "UTF-8"                         -- Set encoding
+vim.opt.isfname:append("@-@")
+
+-- Split behavior
+vim.opt.splitbelow = true                          -- Horizontal splits go below
+vim.opt.splitright = true                          -- Vertical splits go right
+
+-- Key mappings
+vim.g.mapleader = " "                              -- Set leader key to space
+vim.g.maplocalleader = " "                         -- Set local leader key (NEW)
+--
+-- Normal mode mappings
+vim.keymap.set("n", "<leader>c", ":nohlsearch<CR>", { desc = "Clear search highlights" })
+
+-- Center screen when jumping
+vim.keymap.set("n", "n", "nzzzv", { desc = "Next search result (centered)" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Previous search result (centered)" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Half page down (centered)" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Half page up (centered)" })
+
+-- Delete without yanking
+vim.keymap.set({ "n", "v" }, "<leader>d", '"_d', { desc = "Delete without yanking" })
+
+-- Close buffers
+vim.keymap.set("n", "<S-q>", "<cmd>bdelete!<CR>", { desc = "Close buffer" })
+
+-- Buffer navigation
+vim.keymap.set("n", "<S-l>", ":bnext<CR>", { desc = "Next buffer" })
+vim.keymap.set("n", "<S-h>", ":bprevious<CR>", { desc = "Previous buffer" })
+vim.keymap.set("n", "<leader>bn", ":bnext<CR>", { desc = "Next buffer" })
+vim.keymap.set("n", "<leader>bp", ":bprevious<CR>", { desc = "Previous buffer" })
+
+-- Better window navigation
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to bottom window" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to top window" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
+
+-- Splitting & Resizing
+vim.keymap.set("n", "<leader>sv", ":vsplit<CR>", { desc = "Split window vertically" })
+vim.keymap.set("n", "<leader>sh", ":split<CR>", { desc = "Split window horizontally" })
+vim.keymap.set("n", "<C-Up>", ":resize +2<CR>", { desc = "Increase window height" })
+vim.keymap.set("n", "<C-Down>", ":resize -2<CR>", { desc = "Decrease window height" })
+vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", { desc = "Decrease window width" })
+vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", { desc = "Increase window width" })
+
+-- Better indenting in visual mode
+vim.keymap.set("v", "<", "<gv", { desc = "Indent left and reselect" })
+vim.keymap.set("v", ">", ">gv", { desc = "Indent right and reselect" })
+
+-- Move text up and down
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down" })
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line up" })
+vim.keymap.set("v", "<J>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+vim.keymap.set("v", "<K>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+
+-- Quick file navigation
+vim.keymap.set("n", "<leader>e", ":Explore<CR>", { desc = "Open file explorer" })
+vim.keymap.set("n", "<leader>ff", ":find ", { desc = "Find file" })
+
+-- Better J behavior
+vim.keymap.set("n", "J", "mzJ`z", { desc = "Join lines and keep cursor position" })
+
+-- Command-line completion
+vim.opt.wildmenu = true
+vim.opt.wildmode = "longest:full,full"
+vim.opt.wildignore:append({ "*.o", "*.obj", "*.pyc", "*.class", "*.jar" })
+
+-- Better diff options
+vim.opt.diffopt:append("linematch:60")
+
+-- Performance improvements
+-- vim.opt.redrawtime = 10000
+-- vim.opt.maxmempattern = 20000
+
+-- ============================================================================
+-- USEFUL FUNCTIONS
+-- ============================================================================
+
+-- Copy Full File-Path
+vim.keymap.set("n", "<leader>pa", function()
+	local path = vim.fn.expand("%:p")
+	vim.fn.setreg("+", path)
+	print("file:", path)
+end)
+
+-- Basic autocommands
+local augroup = vim.api.nvim_create_augroup("UserConfig", {})
+
+-- Highlight yanked text
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = augroup,
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- Return to last edit position when opening files
+vim.api.nvim_create_autocmd("BufReadPost", {
+  group = augroup,
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
+-- Create undo directory if it doesn't exist
+local undodir = vim.fn.expand("~/.vim/undodir")
+if vim.fn.isdirectory(undodir) == 0 then
+  vim.fn.mkdir(undodir, "p")
+end
+
+-- ============================================================================
+-- STATUSLINE
+-- ============================================================================
+
+-- Git branch function
+local function git_branch()
+  local branch = vim.fn.system("git branch --show-current 2>/dev/null | tr -d '\n'")
+  if branch ~= "" then
+    return "  " .. branch .. " "
+  end
+  return ""
+end
+
+-- File type with icon
+local function file_type()
+  local ft = vim.bo.filetype
+  local icons = {
+    lua = "[LUA]",
+    python = "[PY]",
+    javascript = "[JS]",
+    html = "[HTML]",
+    css = "[CSS]",
+    json = "[JSON]",
+    markdown = "[MD]",
+    vim = "[VIM]",
+    sh = "[SH]",
+  }
+
+  if ft == "" then
+    return "  "
+  end
+
+  return (icons[ft] or ft)
+end
+
+-- LSP status
+local function lsp_status()
+  local clients = vim.lsp.get_clients({ bufnr = 0 })
+  if #clients > 0 then
+    return "  LSP "
+  end
+  return ""
+end
+
+-- Word count for text files
+local function word_count()
+  local ft = vim.bo.filetype
+  if ft == "markdown" or ft == "text" or ft == "tex" then
+    local words = vim.fn.wordcount().words
+    return "  " .. words .. " words "
+  end
+  return ""
+end
+
+-- File size
+local function file_size()
+  local size = vim.fn.getfsize(vim.fn.expand('%'))
+  if size < 0 then return "" end
+  if size < 1024 then
+    return size .. "B "
+  elseif size < 1024 * 1024 then
+    return string.format("%.1fK", size / 1024)
+  else
+    return string.format("%.1fM", size / 1024 / 1024)
+  end
+end
+
+-- Mode indicators with icons
+local function mode_icon()
+  local mode = vim.fn.mode()
+  local modes = {
+    n = "NORMAL",
+    i = "INSERT",
+    v = "VISUAL",
+    V = "V-LINE",
+    ["\22"] = "V-BLOCK",  -- Ctrl-V
+    c = "COMMAND",
+    s = "SELECT",
+    S = "S-LINE",
+    ["\19"] = "S-BLOCK",  -- Ctrl-S
+    R = "REPLACE",
+    r = "REPLACE",
+    ["!"] = "SHELL",
+    t = "TERMINAL"
+  }
+  return modes[mode] or "  " .. mode:upper()
+end
+
+_G.mode_icon = mode_icon
+_G.git_branch = git_branch
+_G.file_type = file_type
+_G.file_size = file_size
+_G.lsp_status = lsp_status
+
+vim.cmd([[
+  highlight StatusLineBold gui=bold cterm=bold
+]])
+
+-- Function to change statusline based on window focus
+local function setup_dynamic_statusline()
+  vim.api.nvim_create_autocmd({"WinEnter", "BufEnter"}, {
+    callback = function()
+    vim.opt_local.statusline = table.concat {
+      "  ",
+      "%#StatusLineBold#",
+      "%{v:lua.mode_icon()}",
+      "%#StatusLine#",
+      " │ %f %h%m%r",
+      "%{v:lua.git_branch()}",
+      " │ ",
+      "%{v:lua.file_type()}",
+      " | ",
+      "%{v:lua.file_size()}",
+      " | ",
+      "%{v:lua.lsp_status()}",
+      "%=",                     -- Right-align everything after this
+      "%l:%c  %P ",             -- Line:Column and Percentage
+    }
+    end
+  })
+  vim.api.nvim_set_hl(0, "StatusLineBold", { bold = true })
+
+  vim.api.nvim_create_autocmd({"WinLeave", "BufLeave"}, {
+    callback = function()
+      vim.opt_local.statusline = "  %f %h%m%r │ %{v:lua.file_type()} | %=  %l:%c   %P "
+    end
   })
 end
-vim.opt.rtp:prepend(lazypath)
-vim.cmd.colorscheme("sorbet")
-vim.g.have_nerd_font = true
 
-require("lazy").setup("plugins")
+setup_dynamic_statusline()
 
-require("options")
-require("keymaps")
-require("filetypes")
+-- ============================================================================
+-- LSP 
+-- ============================================================================
 
--- local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
---
--- vim.filetype.add({
---   extension = {
---     st = "st",
---   },
--- })
+-- Function to find project root
+local function find_root(patterns)
+  local path = vim.fn.expand('%:p:h')
+  local root = vim.fs.find(patterns, { path = path, upward = true })[1]
+  return root and vim.fn.fnamemodify(root, ':h') or path
+end
 
--- parser_config.st = {
---   install_info = {
---     url = "/home/kasperw/repos/tree-sitter-structured-text",   -- local path or git repo
---     files = { "src/parser.c" },             -- note that some parsers also require src/scanner.c or src/scanner.cc
---     -- optional entries:
---     branch = "main",                        -- default branch in case of git repo if different from master
---     -- generate_requires_npm = false,          -- if stand-alone parser without npm dependencies
---     -- requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
---   },
---   filetype = "st",                          -- if filetype does not match the parser name
--- }
---
--- vim.treesitter.language.register('st', 'st')  -- the someft filetype will use the python parser and queries.
---
--- vim.filetype.add({
---   extension = {
---     c3 = "c3",
---     c3i = "c3",
---     c3t = "c3",
---   },
--- })
---
--- parser_config.c3 = {
---   install_info = {
---     url = "https://github.com/c3lang/tree-sitter-c3",
---     files = {"src/parser.c", "src/scanner.c"},
---     branch = "main",
---   },
--- }
+-- lua LSP setup
+local function setup_luals_lsp()
+    vim.lsp.start({
+        name = 'luals',
+        cmd = {'lua-language-server'},
+        filetypes = {'lua'},
+        root_dir = find_root({'.luarc.json', '.luarc.jsonc', '.luacheckrc', '.stylua.toml', 'stylua.toml', 'selene.toml', 'selene.yml', '.git'}),
+        root_markers = find_root({'.luarc.json', '.luarc.jsonc', '.luacheckrc', '.stylua.toml', 'stylua.toml', 'selene.toml', 'selene.yml', '.git'}),
+        settings = {
+            luals = {
+                workspace = {
+                    checkThirdParty = false,
+                    library = {
+                        vim.env.VIMRUNTIME
+                    }
+               }
+           }
+       }
+    })
+end
 
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'lua',
+  callback = setup_luals_lsp,
+  desc = 'Start Lua LSP'
+})
 
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
+-- LSP keymaps 
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(event)
+    local opts = {buffer = event.buf}
+
+    -- Navigation
+    vim.keymap.set('n', 'gD', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'gs', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+
+    -- Information
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+
+    -- Code actions
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set('n', '<leader>fm', vim.lsp.buf.format, opts)
+
+    -- Diagnostics
+    vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, opts)
+    vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
+  end,
+})
+
+-- Better LSP UI
+vim.diagnostic.config({
+  virtual_text = { prefix = '●' },
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
+})
+
+vim.diagnostic.config({
+  signs = {
+    text = {
+        [vim.diagnostic.severity.ERROR] = '󰅚 ',
+        [vim.diagnostic.severity.WARN] = '󰀪 ',
+        [vim.diagnostic.severity.INFO] = '󰋽 ',
+        [vim.diagnostic.severity.HINT] = '󰌶 ',
+    }
+  }
+})
+
+-- formatting
+local function format_code()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local filename = vim.api.nvim_buf_get_name(bufnr)
+  local filetype = vim.bo[bufnr].filetype
+
+  -- Save cursor position
+  local cursor_pos = vim.api.nvim_win_get_cursor(0)
+
+  if filetype == 'python' or filename:match('%.py$') then
+    if filename == '' then
+      print("Save the file first before formatting Python")
+      return
+    end
+
+    local black_cmd = "black --quiet " .. vim.fn.shellescape(filename)
+    local black_result = vim.fn.system(black_cmd)
+
+    if vim.v.shell_error == 0 then
+      vim.cmd('checktime')
+      vim.api.nvim_win_set_cursor(0, cursor_pos)
+      print("Formatted with black")
+      return
+    else
+      print("No Python formatter available (install black)")
+      return
+    end
+  end
+
+  if filetype == 'sh' or filetype == 'bash' or filename:match('%.sh$') then
+    local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+    local content = table.concat(lines, '\n')
+
+    local cmd = {'shfmt', '-i', '2', '-ci', '-sr'}  -- 2 spaces, case indent, space redirects
+    local result = vim.fn.system(cmd, content)
+
+    if vim.v.shell_error == 0 then
+      local formatted_lines = vim.split(result, '\n')
+      if formatted_lines[#formatted_lines] == '' then
+        table.remove(formatted_lines)
+      end
+      vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, formatted_lines)
+      vim.api.nvim_win_set_cursor(0, cursor_pos)
+      print("Shell script formatted with shfmt")
+      return
+    else
+      print("shfmt error: " .. result)
+      return
+    end
+  end
+
+  print("No formatter available for " .. filetype)
+end
+
+vim.api.nvim_create_user_command("FormatCode", format_code, {
+  desc = "Format current file"
+})
+
+vim.keymap.set('n', '<leader>fm', format_code, { desc = 'Format file' })
+
+vim.api.nvim_create_user_command('LspInfo', function()
+  local clients = vim.lsp.get_clients({ bufnr = 0 })
+  if #clients == 0 then
+    print("No LSP clients attached to current buffer")
+  else
+    for _, client in ipairs(clients) do
+      print("LSP: " .. client.name .. " (ID: " .. client.id .. ")")
+    end
+  end
+end, { desc = 'Show LSP client info' })
