f6fa6752173b6df44c0c4729f1b1d4cc7d28f4a7
Kasper Wind <kasperwind79@gmail.com>
2025-08-24 22:19:52 +0200

    Adding rust specific run and build, using secondary buffer

--- lua/lsp_server_settings.lua
+++ lua/lsp_server_settings.lua
@@ -37,9 +37,10 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
         if client ~= nil then
             if client.name == 'rust_analyzer' then
-                vim.keymap.set("n", "<leader>bb", "<cmd>make build<CR>", opts("Cargo build"))
-                vim.keymap.set("n", "<leader>br", "<cmd>make run<CR>", opts("Cargo run"))
-                vim.keymap.set("n", "<leader>bt", "<cmd>make test<CR>", opts("Cargo test"))
+                require('rust')
+                vim.keymap.set("n", "<leader>bb", "<cmd>CargoBuild<CR>", opts("Cargo build"))
+                vim.keymap.set("n", "<leader>br", "<cmd>CargoRun<CR>", opts("Cargo run"))
+                vim.keymap.set("n", "<leader>bt", "<cmd>CargoTest<CR>", opts("Cargo test"))
 
                 local path = client.workspace_folders[1].name .. "/.cargo/config.toml"
                 local filereadable = vim.fn.filereadable(path)
---
+++ lua/rust.lua
@@ -0,0 +1,15 @@
+vim.api.nvim_create_user_command('CargoBuild', function()
+    vim.cmd('enew')
+    vim.cmd('term cargo build')
+end, {})
+
+vim.api.nvim_create_user_command('CargoRun', function()
+    vim.cmd('enew')
+    vim.cmd('term cargo run')
+end, {})
+
+vim.api.nvim_create_user_command('CargoTest', function()
+    vim.cmd('enew')
+    vim.cmd('term cargo test')
+end, {})
+
