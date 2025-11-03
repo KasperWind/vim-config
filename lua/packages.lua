97ada7abe460311fc48c93fd49beb518fdf7f317
Kasper Wind <kasperwind79@gmail.com>
2025-11-03 23:05:53 +0100

    fix no version

--- lua/packages.lua
+++ lua/packages.lua
@@ -33,7 +33,7 @@ vim.pack.add({
     { src = 'https://github.com/j-hui/fidget.nvim.git' },
     { src = 'https://github.com/folke/which-key.nvim.git' },
     { src = 'https://github.com/folke/todo-comments.nvim.git' },
-    { src = 'https://github.com/Saghen/blink.cmp.git' , version="1.*"},
+    { src = 'https://github.com/Saghen/blink.cmp.git'},
     { src = 'https://github.com/xzbdmw/colorful-menu.nvim.git' },
     { src = 'https://github.com/lewis6991/gitsigns.nvim.git' },
     { src = 'https://github.com/sindrets/diffview.nvim.git' },
