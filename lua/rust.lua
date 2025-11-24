ab0f55bedce0c3e687f8186643ace656a5f61530
Kasper Wind <kasperwind79@gmail.com>
2025-11-24 15:41:09 +0100

    adding rust bin call for files in bin folder

--- lua/rust.lua
+++ lua/rust.lua
@@ -4,8 +4,20 @@ vim.api.nvim_create_user_command('CargoBuild', function()
 end, {})
 
 vim.api.nvim_create_user_command('CargoRun', function()
-    vim.cmd('enew')
-    vim.cmd('term cargo run')
+    local filepath = vim.api.nvim_buf_get_name(0)
+    local dir = vim.fs.dirname(filepath)
+    local pos = string.len(dir) - string.find(string.reverse(dir), '/') + 2
+    local current = string.sub(dir, pos)
+    if current == 'bin' then
+        local file = vim.fs.basename(filepath)
+        pos = string.find(file, "%.") - 1
+        local mod_name = string.sub(file, 0, pos)
+        vim.cmd('enew')
+        vim.cmd('term cargo run --bin ' .. mod_name)
+    else
+        vim.cmd('enew')
+        vim.cmd('term cargo run')
+    end
 end, {})
 
 vim.api.nvim_create_user_command('CargoTest', function()
