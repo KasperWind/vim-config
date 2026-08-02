fa6d2264841032c22873d5673317f4fbca8b39fc
Kasper Wind <kasperwind79@gmail.com>
2026-08-02 20:09:44 +0200

    fixed deprecated highlight

--- lua/functions.lua
+++ lua/functions.lua
@@ -26,7 +26,7 @@ local augroup = vim.api.nvim_create_augroup("UserConfig", {})
 vim.api.nvim_create_autocmd("TextYankPost", {
 	group = augroup,
 	callback = function()
-		vim.highlight.on_yank()
+		vim.hl.hl_op(nil)
 	end,
 })
 
